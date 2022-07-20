8fcd86250f517b501089a314b7b7f9b41ce041b0
Yuliy Bugarin <bugarindev@yahoo.com>
2022-07-20 14:53:22 +0800

    feat: created base interface

---
+++ src/LinkInterface.php
@@ -0,0 +1,18 @@
+<?php
+namespace Bugarinov\ChainValidation;
+
+/**
+ * Interface for a chain link
+ * 
+ * @package  Bugarinov\ChainValidation
+ * @author   Yuliy Bugarinov <bugarindev@yahoo.com>
+ * @version  0.1.4
+ * @access   public
+ */
+interface LinkInterface
+{
+    function hasError(): bool;
+    function getError(): ?string;
+    function getErrorCode(): int;
+    function throwError(string $message, int $errorCode = 0);
+}
