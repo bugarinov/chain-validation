e2230cf4368f96617e1273b193c08e3a51750a4a
Yuliy Bugarin <bugarindev@yahoo.com>
2022-07-21 14:48:33 +0800

    feat: created class for handling evaluation results

---
+++ src/EvaluationResult.php
@@ -0,0 +1,62 @@
+<?php
+namespace Bugarinov\ChainValidation;
+
+/**
+ * A class for handles reults of evaluation after AbstractLink::evaluate() is called
+ * 
+ * @package  Bugarinov\ChainValidation
+ * @author   Yuliy Bugarin <bugarindev@yahoo.com>
+ * @version  0.2.0
+ * @access   public
+ */
+class EvaluationResult
+{
+    /**
+     * @var ?array
+     */
+    private $evaluatedData;
+
+    /**
+     * @var bool
+     */
+    private $hasError;
+
+    /**
+     * @var int
+     */
+    private $errorCode;
+
+    /**
+     * @var string
+     */
+    private $errorMessage;
+
+    public function __construct(?array $evaluatedData, string $errorMessage = null, int $errorCode = 0)
+    {
+        $this->evaluatedData = $evaluatedData;
+        $this->errorMessage = $errorMessage;
+        $this->errorCode = $errorCode;
+
+        $this->hasError = $this->errorCode != 0 || $this->errorMessage !== null;
+    }
+
+    public function hasError(): bool
+    {
+        return $this->hasError;
+    }
+
+    public function getErrorCode(): int
+    {
+        return $this->errorCode;
+    }
+
+    public function getErrorMessage(): string
+    {
+        return $this->errorMessage;
+    }
+
+    public function getEvaluatedData(): ?array
+    {
+        return $this->evaluatedData;
+    }
+}
