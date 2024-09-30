841a327be86f68b155139b5b618d1df775857d1a
Yuliy Bugarin <bugarindev@yahoo.com>
2024-09-30 16:49:57 +0800

    feat: added constructor param errorBody

--- src/ResultFailed.php
+++ src/ResultFailed.php
@@ -3,8 +3,8 @@ namespace Bugarinov\ChainValidation;
 
 class ResultFailed extends EvaluationResult
 {
-    public function __construct(string $errorMessage, int $errorCode = 0)
+    public function __construct(string $errorMessage, int $errorCode = 0, ?array $errorBody = null)
     {
-        parent::__construct(null, $errorMessage, $errorCode);
+        parent::__construct(null, $errorMessage, $errorCode, $errorBody);
     }
 }
